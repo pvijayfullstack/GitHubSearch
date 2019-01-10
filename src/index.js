cf1477e820dd5d24c873ba90bbd851970ef2a903
David A Pears <davidapears@gmail.com>
2019-01-10 11:54:17 +0000

    Functionality working, adds CSS/Frontend layout

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './containers/App';
-import * as serviceWorker from './createServiceWorker';
+import { render } from 'react-dom';
+import Root from './containers/Root';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(<Root />, document.getElementById('root'));
